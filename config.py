e615d9c7c584798f5ec107864c0c9b1d46734d63
AngryCatz <120298303+AngryCatz@users.noreply.github.com>
2024-01-02 23:08:08 +0530

    config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-TOKEN = os.environ.get("TOKEN", None) # Dapatkan di @botfather
-OWNER = os.environ.get("OWNER", "kenkanasw") # Isi dengan username kalian tanpa tanda @
+TOKEN = os.environ.get("TOKEN", "6316649627:AAFw3R--P-KL1_fYsHr7jgWJuTLyuuqcvwY") # Dapatkan di @botfather
+OWNER = os.environ.get("OWNER", "nowbusy") # Isi dengan username kalian tanpa tanda @
 GROUP = os.environ.get("GROUP", "aruna_mutual") # Isi dengan username group kalian tanpa tanda @ kalau gak punya gak usah isi
 CHANNEL = os.environ.get("CHANNEL", "gabutan_escape") # Isi dengan username channel kalian tanpa tanda @ kalau gak punya gak usah isi
-DB_URI = os.environ.get("DATABASE_URL", "")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://AutoTest:AutoTest@cluster0.ixudnhq.mongodb.net/?retryWrites=true&w=majority")
